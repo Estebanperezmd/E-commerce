8888f135e7a9423991e3708d0d100fdbd6e70ae2
Jhoreinis Anaya <91376980+Jhoreinis@users.noreply.github.com>
2025-09-29 23:57:41 -0500

    Update Pago.ts

--- Backend/services/pagos/domain/entities/Pago.ts
+++ Backend/services/pagos/domain/entities/Pago.ts
@@ -1,9 +1,27 @@
-export class Pago {
-  constructor(
-    public id: number | null,
-    public monto: number,
-    public metodo: string,
-    public fechaPago: Date | null = null,
-  ) {}
-}
+import { ApiProperty } from '@nestjs/swagger';
 
+export class Pago {
+  @ApiProperty({ example: 1 })
+  id: number | null;
+
+  @ApiProperty({ example: 250.75 })
+  monto: number;
+
+  @ApiProperty({ example: 'tarjeta_credito' })
+  metodo: string;
+
+  @ApiProperty({ example: '2025-09-29T20:00:00Z' })
+  fechaPago: Date | null;
+
+  constructor(
+    id: number | null,
+    monto: number,
+    metodo: string,
+    fechaPago: Date | null = null,
+  ) {
+    this.id = id;
+    this.monto = monto;
+    this.metodo = metodo;
+    this.fechaPago = fechaPago;
+  }
+}
