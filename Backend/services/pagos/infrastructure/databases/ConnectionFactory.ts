6761a019098efb8cb398b9daa917187681ee4177
Jhoreinis Anaya <91376980+Jhoreinis@users.noreply.github.com>
2025-09-30 00:28:11 -0500

    Update ConnectionFactory.ts

--- Backend/services/pagos/infrastructure/databases/ConnectionFactory.ts
+++ Backend/services/pagos/infrastructure/databases/ConnectionFactory.ts
@@ -1,1 +1,17 @@
-// TODO: factor�a para crear conexiones
+import { DataSource } from 'typeorm';
+import { databaseConfig } from './DatabaseConfig';
+
+export const AppDataSource = new DataSource(databaseConfig);
+
+export const initDatabase = async () => {
+  try {
+    if (!AppDataSource.isInitialized) {
+      await AppDataSource.initialize();
+      console.log('📦 Database connected successfully');
+    }
+  } catch (error) {
+    console.error('❌ Error connecting to database:', error);
+    throw error;
+  }
+};
+
