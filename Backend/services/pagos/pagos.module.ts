39224b5b3ee6188053e0b671f4203541e076ae43
Jhoreinis Anaya <91376980+Jhoreinis@users.noreply.github.com>
2025-09-30 00:30:09 -0500

    Update pagos.module.ts

--- Backend/services/pagos/pagos.module.ts
+++ Backend/services/pagos/pagos.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { PagoController } from './interfaces/controllers/PagoController';
 import { PaymentService } from './application/services/PaymentService';
+import { Pago } from './domain/entities/Pago';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([Pago]), // 👈 agrega la entidad Pago
+  ],
   controllers: [PagoController],
   providers: [PaymentService],
   exports: [PaymentService],
