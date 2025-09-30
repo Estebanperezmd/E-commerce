de91e9eac98f6a4a8fe2aadc900fa310f1257983
Jhoreinis Anaya <91376980+Jhoreinis@users.noreply.github.com>
2025-09-30 01:03:58 -0500

    Update and rename main.ts to main.js

--- Backend/services/main.js
+++ Backend/services/main.js
@@ -1,5 +1,5 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from './app.module.js';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
@@ -15,5 +15,9 @@ async function bootstrap() {
   SwaggerModule.setup('api', app, document);
 
   await app.listen(3000);
+  console.log('🚀 Servidor corriendo en http://localhost:3000');
+  console.log('📖 Swagger en http://localhost:3000/api');
 }
+
 bootstrap();
+
