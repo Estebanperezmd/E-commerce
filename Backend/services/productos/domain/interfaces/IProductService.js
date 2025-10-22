12d2890390274f4a863f2ac35f3b55a01655c191
Mateo Arcieri <arcierim@uninorte.edu.co>
2025-10-22 00:01:48 -0500

    Cleared redundant classes
    User controler removed from products

---
+++ Backend/services/productos/domain/interfaces/IProductService.js
@@ -0,0 +1,27 @@
+class IProductService {
+  findById(id) {
+    throw new Error('Method not implemented');
+  }
+
+  findAll() {
+    throw new Error('Method not implemented');
+  }
+
+  create(productData) {
+    throw new Error('Method not implemented');
+  }
+
+  update(id, productData) {
+    throw new Error('Method not implemented');
+  }
+
+  updateStock(id, quantity) {
+    throw new Error('Method not implemented');
+  }
+
+  delete(id) {
+    throw new Error('Method not implemented');
+  }
+}
+
+module.exports = IProductService;
