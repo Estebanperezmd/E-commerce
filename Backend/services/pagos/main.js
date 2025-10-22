203c3658b0e1efca9569fed8437521010bb05cf3
Jhoreinis Anaya <91376980+Jhoreinis@users.noreply.github.com>
2025-10-22 00:35:24 -0500

    Create main.js

---
+++ Backend/services/pagos/main.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import { createConnection } from './infrastructure/databases/connection.js';
+import { PagoController } from './interfaces/controllers/PagoController.js';
+
+const app = express();
+app.use(bodyParser.json());
+
+// rutas del microservicio pagos
+const pagoController = new PagoController();
+app.use('/pagos', pagoController.router);
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, async () => {
+  await createConnection();
+  console.log(`💰 Microservicio Pagos corriendo en puerto ${PORT}`);
+});
