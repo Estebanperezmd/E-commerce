5a5b7484511dfd69f01553c8f0fe589ad387978b
Mateo Arcieri <arcierim@uninorte.edu.co>
2025-11-26 02:29:44 -0500

    Update main.js
    Updated Main JS for the port to be assigned correctly on Railway

--- Backend/services/main.js
+++ Backend/services/main.js
@@ -14,9 +14,8 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3000);
-  console.log('🚀 Servidor corriendo en http://localhost:3000');
-  console.log('📖 Swagger en http://localhost:3000/api');
+  await app.listen(process.env.PORT || 3000);
+
 }
 
 bootstrap();
